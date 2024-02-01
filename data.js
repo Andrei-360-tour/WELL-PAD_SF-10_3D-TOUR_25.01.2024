82583b15c7cb52f76a3669aaa1ae30cc02e6d481
Andrei-360-tour <156935416+Andrei-360-tour@users.noreply.github.com>
2024-02-01 10:48:56 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,741 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sf-10-overview-1",
+      "name": "SF-10-Overview 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.08843702467827441,
+        "pitch": 0.13891442475849658,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.27356740968424376,
+          "pitch": -0.12190825994849064,
+          "rotation": 0,
+          "target": "1-sf-10-overview-2"
+        },
+        {
+          "yaw": 0.4811918742380321,
+          "pitch": 0.15559984023233575,
+          "rotation": 0,
+          "target": "8-sf-10-access-road"
+        },
+        {
+          "yaw": 0.0008827964831574775,
+          "pitch": 0.29007715001129597,
+          "rotation": 0,
+          "target": "7-sf-10-sucker-rod-pumping-system-1"
+        },
+        {
+          "yaw": -0.30823597141097636,
+          "pitch": 0.4003895816710763,
+          "rotation": 0,
+          "target": "6-sf-10-old-security-building"
+        },
+        {
+          "yaw": -0.5758976409426388,
+          "pitch": 0.2028507171718168,
+          "rotation": 0,
+          "target": "4-sf-10-wellpad-1"
+        },
+        {
+          "yaw": -0.13627494024021125,
+          "pitch": 0.2775287155158246,
+          "rotation": 0,
+          "target": "2-sf-10-wellheadpipeline"
+        },
+        {
+          "yaw": -0.7954233417282595,
+          "pitch": 0.37119347014989756,
+          "rotation": 0,
+          "target": "5-sf-10-wellpad-2"
+        },
+        {
+          "yaw": 0.2993313795091357,
+          "pitch": 0.13723978100277812,
+          "rotation": 0,
+          "target": "9-sf-10-overhead-line"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sf-10-overview-2",
+      "name": "SF-10-Overview 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.45100595843041624,
+        "pitch": 0.3368729366649674,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1159080833228785,
+          "pitch": 0.06862427912704838,
+          "rotation": 0,
+          "target": "0-sf-10-overview-1"
+        },
+        {
+          "yaw": -0.7496731935239183,
+          "pitch": 0.7317263945428163,
+          "rotation": 0,
+          "target": "9-sf-10-overhead-line"
+        },
+        {
+          "yaw": 0.11340473387172523,
+          "pitch": 0.2441359694783536,
+          "rotation": 0,
+          "target": "3-sf-10-sucker-rod-pumping-system"
+        },
+        {
+          "yaw": 0.5025136875976948,
+          "pitch": 0.21399984663412397,
+          "rotation": 0,
+          "target": "4-sf-10-wellpad-1"
+        },
+        {
+          "yaw": -0.8836673818869016,
+          "pitch": 0.531722633937477,
+          "rotation": 0,
+          "target": "8-sf-10-access-road"
+        },
+        {
+          "yaw": -0.12046731645547126,
+          "pitch": 0.25209532397218304,
+          "rotation": 0,
+          "target": "7-sf-10-sucker-rod-pumping-system-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sf-10-wellheadpipeline",
+      "name": "SF-10-Wellhead&pipeline",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 1.2935703959871887,
+        "pitch": -0.006196351975621184,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5196844175708835,
+          "pitch": 0.03471035761640806,
+          "rotation": 0,
+          "target": "3-sf-10-sucker-rod-pumping-system"
+        },
+        {
+          "yaw": -0.6149268547643114,
+          "pitch": 0.042002670751337234,
+          "rotation": 0,
+          "target": "6-sf-10-old-security-building"
+        },
+        {
+          "yaw": -2.27815266250089,
+          "pitch": 0.42958703518540986,
+          "rotation": 0,
+          "target": "7-sf-10-sucker-rod-pumping-system-1"
+        },
+        {
+          "yaw": 0.8672438361276544,
+          "pitch": -0.062350315397978306,
+          "rotation": 0,
+          "target": "4-sf-10-wellpad-1"
+        },
+        {
+          "yaw": -0.6990863217352299,
+          "pitch": -0.40333744129999616,
+          "rotation": 0,
+          "target": "0-sf-10-overview-1"
+        },
+        {
+          "yaw": -2.9446818404359902,
+          "pitch": 0.11233352535478858,
+          "rotation": 0,
+          "target": "8-sf-10-access-road"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-sf-10-sucker-rod-pumping-system",
+      "name": "SF-10-Sucker rod pumping system",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.8326456554672923,
+        "pitch": -0.22850319042542822,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.539948830065983,
+          "pitch": 0.19440083550863108,
+          "rotation": 0,
+          "target": "2-sf-10-wellheadpipeline"
+        },
+        {
+          "yaw": 2.134402550607027,
+          "pitch": 0.0035972865995752557,
+          "rotation": 0,
+          "target": "9-sf-10-overhead-line"
+        },
+        {
+          "yaw": -0.08992294231323505,
+          "pitch": -0.012004784185805306,
+          "rotation": 0,
+          "target": "4-sf-10-wellpad-1"
+        },
+        {
+          "yaw": 1.8865661099718132,
+          "pitch": -0.4107327630101274,
+          "rotation": 0,
+          "target": "1-sf-10-overview-2"
+        },
+        {
+          "yaw": -2.8432456140963698,
+          "pitch": 0.10446576739822788,
+          "rotation": 0,
+          "target": "7-sf-10-sucker-rod-pumping-system-1"
+        },
+        {
+          "yaw": 2.6059147587861826,
+          "pitch": 0.029229240895590536,
+          "rotation": 0,
+          "target": "8-sf-10-access-road"
+        },
+        {
+          "yaw": -1.1118835688912796,
+          "pitch": -0.021294967403655107,
+          "rotation": 0,
+          "target": "5-sf-10-wellpad-2"
+        },
+        {
+          "yaw": -1.8840289668927035,
+          "pitch": 0.03964342102037932,
+          "rotation": 0,
+          "target": "6-sf-10-old-security-building"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-sf-10-wellpad-1",
+      "name": "SF-10-WellPad 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.208707105557675,
+        "pitch": -0.02312608646913361,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1251374791029676,
+          "pitch": -0.013475594428157933,
+          "rotation": 0,
+          "target": "5-sf-10-wellpad-2"
+        },
+        {
+          "yaw": -2.96310254155563,
+          "pitch": 0.07573562541646339,
+          "rotation": 0,
+          "target": "9-sf-10-overhead-line"
+        },
+        {
+          "yaw": -2.3328615376908726,
+          "pitch": 0.12078548897364882,
+          "rotation": 0,
+          "target": "3-sf-10-sucker-rod-pumping-system"
+        },
+        {
+          "yaw": -0.3913944738572752,
+          "pitch": -0.4235523430391517,
+          "rotation": 0,
+          "target": "0-sf-10-overview-1"
+        },
+        {
+          "yaw": -3.11507556585973,
+          "pitch": -0.19186217225709967,
+          "rotation": 0,
+          "target": "1-sf-10-overview-2"
+        },
+        {
+          "yaw": -2.1268552081030627,
+          "pitch": 0.11641640906206874,
+          "rotation": 0,
+          "target": "2-sf-10-wellheadpipeline"
+        },
+        {
+          "yaw": -2.763189727497373,
+          "pitch": 0.06662954673126364,
+          "rotation": 0,
+          "target": "8-sf-10-access-road"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-sf-10-wellpad-2",
+      "name": "SF-10-WellPad 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.111818652196625,
+        "pitch": -0.058273454205334474,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.938754167197679,
+          "pitch": 0.09449224275807566,
+          "rotation": 0,
+          "target": "4-sf-10-wellpad-1"
+        },
+        {
+          "yaw": -1.1625428234294368,
+          "pitch": 0.06634937787327999,
+          "rotation": 0,
+          "target": "6-sf-10-old-security-building"
+        },
+        {
+          "yaw": 0.09761353841281739,
+          "pitch": -0.5149489286900941,
+          "rotation": 0,
+          "target": "0-sf-10-overview-1"
+        },
+        {
+          "yaw": -1.9973019139125263,
+          "pitch": 0.07840673872587445,
+          "rotation": 0,
+          "target": "3-sf-10-sucker-rod-pumping-system"
+        },
+        {
+          "yaw": -1.7244814936576045,
+          "pitch": 0.05275108549393792,
+          "rotation": 0,
+          "target": "7-sf-10-sucker-rod-pumping-system-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-sf-10-old-security-building",
+      "name": "SF-10-Old Security building",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.9927109807183285,
+        "pitch": 0.013891442475848947,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.709029293183457,
+          "pitch": 0.04190629409388791,
+          "rotation": 0,
+          "target": "5-sf-10-wellpad-2"
+        },
+        {
+          "yaw": -1.2862506926491637,
+          "pitch": 0.09546616345758352,
+          "rotation": 0,
+          "target": "7-sf-10-sucker-rod-pumping-system-1"
+        },
+        {
+          "yaw": -1.9120185719737837,
+          "pitch": 0.049765700143677094,
+          "rotation": 0,
+          "target": "3-sf-10-sucker-rod-pumping-system"
+        },
+        {
+          "yaw": 2.1257227347443903,
+          "pitch": -0.5980105689853694,
+          "rotation": 0,
+          "target": "0-sf-10-overview-1"
+        },
+        {
+          "yaw": -0.8427403243824934,
+          "pitch": 0.0016820482190595953,
+          "rotation": 0,
+          "target": "8-sf-10-access-road"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-sf-10-sucker-rod-pumping-system-1",
+      "name": "SF-10-Sucker rod pumping system 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -1.4381997804588877,
+        "pitch": 0.15974509063351405,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4899778640345147,
+          "pitch": -0.034951855428522194,
+          "rotation": 0,
+          "target": "8-sf-10-access-road"
+        },
+        {
+          "yaw": -2.8804993880249796,
+          "pitch": 0.12020715050401343,
+          "rotation": 0,
+          "target": "6-sf-10-old-security-building"
+        },
+        {
+          "yaw": -1.878016221097834,
+          "pitch": 0.1808336662006429,
+          "rotation": 0,
+          "target": "2-sf-10-wellheadpipeline"
+        },
+        {
+          "yaw": 0.15029595864328193,
+          "pitch": -0.047094936635039275,
+          "rotation": 0,
+          "target": "9-sf-10-overhead-line"
+        },
+        {
+          "yaw": 3.124856119647049,
+          "pitch": -0.31769538175540646,
+          "rotation": 0,
+          "target": "0-sf-10-overview-1"
+        },
+        {
+          "yaw": 0.003085576608636842,
+          "pitch": -0.3458176712059746,
+          "rotation": 0,
+          "target": "1-sf-10-overview-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-sf-10-access-road",
+      "name": "SF-10-Access road",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.7173965643591469,
+        "pitch": 0.19100733404293635,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3467060918762783,
+          "pitch": 0.11925230075667592,
+          "rotation": 0,
+          "target": "9-sf-10-overhead-line"
+        },
+        {
+          "yaw": -2.0148529314213555,
+          "pitch": 0.06930626398669304,
+          "rotation": 0,
+          "target": "7-sf-10-sucker-rod-pumping-system-1"
+        },
+        {
+          "yaw": -0.5141521812555698,
+          "pitch": -0.4650729249616923,
+          "rotation": 0,
+          "target": "1-sf-10-overview-2"
+        },
+        {
+          "yaw": -2.258372407724277,
+          "pitch": 0.048522068765198156,
+          "rotation": 0,
+          "target": "6-sf-10-old-security-building"
+        },
+        {
+          "yaw": -1.6021742137688157,
+          "pitch": 0.028547626429944017,
+          "rotation": 0,
+          "target": "4-sf-10-wellpad-1"
+        },
+        {
+          "yaw": -2.5407978921493495,
+          "pitch": -0.4679712587617626,
+          "rotation": 0,
+          "target": "0-sf-10-overview-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-sf-10-overhead-line",
+      "name": "SF-10-OverHead Line",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.6847745815190454,
+        "pitch": -0.029128255761577293,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2165185692364382,
+          "pitch": -0.46021201962239644,
+          "rotation": 0,
+          "target": "1-sf-10-overview-2"
+        },
+        {
+          "yaw": -2.7000317819333333,
+          "pitch": 0.09512010523958381,
+          "rotation": 0,
+          "target": "8-sf-10-access-road"
+        },
+        {
+          "yaw": -1.2608032344219673,
+          "pitch": 0.03689779667367077,
+          "rotation": 0,
+          "target": "3-sf-10-sucker-rod-pumping-system"
+        },
+        {
+          "yaw": -1.8603525475235827,
+          "pitch": -0.4371401354424531,
+          "rotation": 0,
+          "target": "0-sf-10-overview-1"
+        },
+        {
+          "yaw": -0.8442682656608618,
+          "pitch": 0.0030340969216453573,
+          "rotation": 0,
+          "target": "4-sf-10-wellpad-1"
+        },
+        {
+          "yaw": -1.582473542908314,
+          "pitch": 0.046604742847199176,
+          "rotation": 0,
+          "target": "7-sf-10-sucker-rod-pumping-system-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "WellPad SF-10",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
